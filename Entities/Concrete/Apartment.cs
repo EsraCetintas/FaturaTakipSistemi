60b839bf9f09dc0477b7315f62b1dad1cdc7af27
esra.cetintas@outlook.com.tr <esra.cetintas@outlook.com.tr>
2021-11-26 13:06:35 +0300

    Add project files.

---
+++ Entities/Concrete/Apartment.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+   public class Apartment:IEntity
+    {
+        public int Id { get; set; }
+        public int ApartmentTypeId { get; set; }
+        public int UserId { get; set; }
+        public int BlokNo { get; set; }
+        public int FloorNo { get; set; }
+        public bool State { get; set; }
+       
+    }
+}
---
+++ Entities/Concrete/Invoice.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+   public class Invoice:IEntity
+    {
+        public int Id { get; set; }
+        public int ApartmentId { get; set; }
+        public int CategoryId { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
